ee421dbd68d23386b0c7b36792be0520074f9cd2
Kyran3441 <kyran3441@gmail.com>
2025-04-17 05:57:00 +0600

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import LawxiliaryPlatform from './components/custom/LawxiliaryPlatform';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <LawxiliaryPlatform />
+  </React.StrictMode>
+);
